5d4f1f930a2f98d5874e41599641eed4f41fd4f9
SARS-CLASSIFY <1241286547@qq.com>
2020-10-17 20:21:02 +0800

    LAST PUSH

--- 练习1腐蚀与膨胀/源.cpp
+++ 练习1腐蚀与膨胀/源.cpp
@@ -9,8 +9,10 @@ using namespace cv;
 
 int main()
 {
-	Mat srcimg = imread("D://1.jpg");
-
+	Mat srcimg = imread("C://Users//86150//Desktop//1.jpg");
+	Mat grayimg,binaryimg;
+	cvtColor(srcimg, grayimg, CV_BGR2GRAY);
+	threshold(grayimg, binaryimg, 100, 255, THRESH_OTSU);
 	namedWindow("原图", WINDOW_NORMAL);
 	namedWindow("膨胀效果图", WINDOW_NORMAL);
 	namedWindow("腐蚀效果图",WINDOW_NORMAL);
@@ -19,10 +21,10 @@ int main()
 
 	Mat element = getStructuringElement(MORPH_RECT,Size(15,15));
 	Mat out,out1,out2,out3;//膨胀，腐蚀
-	morphologyEx(srcimg,out, MORPH_ERODE,element);//腐蚀运算
-	morphologyEx(srcimg, out1, MORPH_DILATE, element);//膨胀运算
-	morphologyEx(srcimg, out2, MORPH_OPEN, element);//开运算
-	morphologyEx(srcimg, out3, MORPH_CLOSE, element);//开运算
+	morphologyEx(binaryimg,out, MORPH_ERODE,element);//腐蚀运算
+	morphologyEx(binaryimg, out1, MORPH_DILATE, element);//膨胀运算
+	morphologyEx(binaryimg, out2, MORPH_OPEN, element);//开运算
+	morphologyEx(binaryimg, out3, MORPH_CLOSE, element);//开运算
 	imshow("原图",srcimg);
 	imshow("膨胀效果图", out1);
 	imshow("腐蚀效果图", out);
