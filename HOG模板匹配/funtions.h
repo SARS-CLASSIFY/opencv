bd603bfe0bf5d1db6ddfcbaf8c36f51211d28dee
SARS-CLASSIFY <1241286547@qq.com>
2020-12-30 12:28:26 +0800

    本程序包含2功能，第一个segColor()实现Kmeans的视频抠图对应附加题，Kmeansdemo()实现Kmeans的简单是示范。

--- HOG模板匹配/funtions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#pragma once
-#include <opencv2/opencv.hpp>
-using namespace cv;
-
-int compareImages();
-int calcHOG(cv::Mat src, float* hist, int nAngle, int cellSize);
-float normL2(float* Hist1, float* Hist2, int size);
